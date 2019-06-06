216cca9c07535f6d335b36de083b6f4cb756b223
Dale Gordon <dale@avochoc.com>
2019-06-06 20:54:58 +0200

    Operators #done

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,8 +1,18 @@
 import React, {useState} from 'react';
+
+//Defining an immutable list of operators
+const operatorsEnum = Object.freeze({ADD: 1, SUBTRACT: 2, MULTIPLY: 3, DIVIDE: 4, EQUALS: 5});
+
+//Hold the last used operator in memory
+let lastOperator = operatorsEnum.ADD;
+
+//Keep a reference to the last used items so that you can spam the equals
+let memoryOperator = 0;
+
 const Calculator = () => {
     const buttons = [
         new dataButton("clear-entry", "CE" , () => clearDisplay()),
-        new dataButton("clear", "C", () => {clearDisplay(); setAnswer(0);}),
+        new dataButton("clear", "C", () => {clearDisplay(); setAnswer(0); lastOperator = operatorsEnum.ADD}),
         new dataButton("backspace", "<-", () => setDisplay(display.slice(0, -1))),
         new dataButton("divide", "/", () => {operate(operatorsEnum.DIVIDE)}),
         new dataButton("seven", "7", () => setDisplay(`${display}7`), "Number"),
@@ -19,44 +29,48 @@ const Calculator = () => {
         new dataButton("add", "+", () => {operate(operatorsEnum.ADD)}),
         new dataButton("negate", "+/-", () => setDisplay(display[0] === '-' ? display.slice(1) : `-${display}`)),
         new dataButton("zero", "0", () => setDisplay(`${display}0`), "Number"),
-        new dataButton("decimal-point", ",", value => `${value},`),
-        new dataButton("equals", "=", () => setAnswer('42')),
+        new dataButton("decimal-point", ".", () => { if(display.slice(-1) !== '.') setDisplay(display.length > 0 ? `${display}.` : `0.`)}),
+        new dataButton("equals", "=", () => operate(operatorsEnum.EQUALS)),
+        //When last operator is equals the next non equal operator must:
+        //a) clear the display
+        //b) set itself as the last operator
     ];
 
     //Helper functions
-    const clearDisplay = () => {setDisplay('')};
-
-    //Defining an immutable list of operators
-    const operatorsEnum = Object.freeze({ADD: 1, SUBTRACT: 2, MULTIPLY: 3, DIVIDE: 4});
+    const clearDisplay = () => {
+        setDisplay('')
+    };
 
     //Defining state
     const [answer, setAnswer] = useState(0);
     const [display, setDisplay] = useState('');
 
-    //Hold the last used operator in memory
-    let lastOperator = operatorsEnum.ADD;
-
     //Operator functionality
     const operate = (operator) => {
         //Apply the last operator on the running total
-        switch(lastOperator) {
-            case operatorsEnum.ADD:
-                setAnswer(answer + parseFloat(display));
-                break;
-            case operatorsEnum.SUBTRACT:
-                setAnswer(answer - parseFloat(display));
-                break;
-            case operatorsEnum.MULTIPLY:
-                setAnswer(answer * parseFloat(display));
-                break;
-            case operatorsEnum.DIVIDE:
-                setAnswer(answer / parseFloat(display));
-                break;
-            default: {
+        if (display.length > 0) {
+            switch (lastOperator) {
+                case operatorsEnum.ADD:
+                    setAnswer(answer + parseFloat(display));
+                    break;
+                case operatorsEnum.SUBTRACT:
+                    setAnswer(answer - parseFloat(display));
+                    break;
+                case operatorsEnum.MULTIPLY:
+                    setAnswer(answer * parseFloat(display));
+                    break;
+                case operatorsEnum.DIVIDE:
+                    setAnswer(answer / parseFloat(display));
+                    break;
+                case operatorsEnum.EQUALS:
+
+                    break;
+                default: {
+                }
             }
+            clearDisplay();
+            lastOperator = operator;
         }
-        clearDisplay();
-        lastOperator = operator;
     };
 
     //Render Method
@@ -73,7 +87,8 @@ const Calculator = () => {
             <div className="row">
                 {buttons.map(button => {
                     return (
-                        <div className={`Button col-3 p-3 ${button.styling}`} onClick={() => button.callback()}>
+                        <div className={`Button col-3 p-3 ${button.styling}`} onClick={() => button.callback()}
+                             key={button.key}>
                             <div className="button-inner">
                                 <h1>{button.text}</h1>
                             </div>
